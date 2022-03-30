0420876c0efbd7a862b22d1384cb42521f296066
Ekunola Paul Oluwapelumi <pscon@Ekunolas-MacBook-Pro.local>
2022-03-30 09:35:28 +0100

    adding files

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Learn to design and code React Apps</h1>
+    <p>Complete courses about the best tool and design systems. Prototypes and build apps with React </p>
+   
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+      <Link to="/using-dsg">Go to "Using DSG"</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
